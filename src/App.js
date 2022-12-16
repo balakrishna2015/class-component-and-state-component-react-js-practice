51a993a0ae9154fb83df075edaab00c83b1e6330
balakrishna2015 <vellabalakrishna@gmail.com>
2022-12-16 11:09:59 +0000

    pracice only

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Counter from './components/Counter'
+
+const App = () => <Counter />
+
+export default App
+
+// import Welcome from './components/Welcome'
+
+// const App = () => {
+//   return <Welcome name="Bala" />
+// }
+// export default App
